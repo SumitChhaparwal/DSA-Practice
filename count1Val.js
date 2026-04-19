e8528e9aacd22df30ef4f4ad62b777abac097853
SumitChaparwal <chhaparwalsumit33@gmail.com>
2026-04-20 01:22:34 +0530

    Improved runtime

--- count1Val.js
+++ count1Val.js
@@ -1,17 +1,14 @@
-arr = [0, 0, 0, 0, 0, 1, 1, 1, 1];
+arr = [0, 0, 0, 0, 0, 1, 1];
 let left = 0;
-let count = 0;
 let right = arr.length;
-
 function countValue() {
   while (left < right) {
     let mid = left + Math.floor((right - left) / 2);
-    if (arr[mid] === 0) {
-      left += 1;
-    }
     if (arr[mid] === 1) {
-      count += 1;
-      right -= 1;
+      right = mid;
+      return arr.length - mid;
+    } else {
+      left++;
     }
   }
   return count;
@@ -19,3 +16,6 @@ function countValue() {
 
 let result = countValue();
 console.log("Total 1s:", result);
+
+//Time Complexity: O(log n)
+//Space Complexity: O(1)
